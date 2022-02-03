6fc8eed40e64dc389e20deb9e59aa431f2d6b7c9
unknown <okpara_tochukwu@yahoo.com>
2022-02-03 09:22:02 +0100

    created solver method

--- solver.rb
+++ solver.rb
@@ -1,7 +1,27 @@
 class Solver
-  def factorial(num); end
-
-  def reverse(str); end
-
-  def fizzbuzz(num); end
+  def factorial(num)
+    raise 'Number is negative' if num.negative?
+    val = 1
+    while num > 0
+      val *= num
+      num -= 1 
+    end
+    return val
 end
+  def reverse(str)
+ return    str.chars.reverse.join
+
+  end
+  def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 ==0
+    return  'fizzbuzz'
+    elsif num % 3 == 0
+    return   'fizz'
+    elsif num % 5 == 0
+       return 'buzz'
+    else
+     return  num
+    end
+  end
+end
+
